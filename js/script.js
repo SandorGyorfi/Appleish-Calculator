73c68a08a15fca98c81bcb76ba577a4f6976370b
Sandor Gyorfi <mr.sandorgyorfi@gmail.com>
2023-11-01 11:06:43 +0000

    Add
    Added calculator logic with scientific functions.

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ let currentOperation = null;
 
 document.addEventListener('DOMContentLoaded', () => {
     const display = document.querySelector('#display');
-    const buttons = document.querySelectorAll('#buttons button');
+    const buttons = document.querySelectorAll('button'); // Select all buttons
 
     buttons.forEach(button => {
         button.addEventListener('click', () => {
@@ -33,38 +33,84 @@ document.addEventListener('DOMContentLoaded', () => {
                     computeResult();
                 }
                 break;
+            case 'function':
+                handleScientificFunction(value);
+                break;
+            case 'constant':
+                handleConstant(value);
+                break;
         }
         updateDisplay();
     }
 
-    function updateDisplay() {
-        display.textContent = currentInput || '0';
+    function handleScientificFunction(func) {
+        const currentNum = parseFloat(currentInput);
+        let result;
+
+        switch (func) {
+            case 'sin': result = Math.sin(currentNum); break;
+            case 'cos': result = Math.cos(currentNum); break;
+            case 'tan': result = Math.tan(currentNum); break;
+            case 'asin': result = Math.asin(currentNum); break;
+            case 'acos': result = Math.acos(currentNum); break;
+            case 'atan': result = Math.atan(currentNum); break;
+            case 'log': result = Math.log10(currentNum); break;
+            case 'ln': result = Math.log(currentNum); break;
+            case 'exp': result = Math.exp(currentNum); break;
+            case 'sqrt': result = Math.sqrt(currentNum); break;
+            case 'square': result = Math.pow(currentNum, 2); break;
+            case 'cube': result = Math.pow(currentNum, 3); break;
+            case 'inv': result = 1 / currentNum; break;
+            case 'abs': result = Math.abs(currentNum); break;
+            case 'fact': result = factorial(currentNum); break;
+            case 'percent': result = currentNum / 100; break;
+            case 'pow':
+                currentOperation = 'pow';
+                previousInput = currentInput;
+                currentInput = '';
+                return; 
+        }
+
+        currentInput = result.toString();
+        previousInput = "";
+        currentOperation = null;
+    }
+
+    function handleConstant(constant) {
+        switch (constant) {
+            case 'pi': currentInput = Math.PI.toString(); break;
+            case 'e': currentInput = Math.E.toString(); break;
+        }
     }
 
     function computeResult() {
-        if (previousInput && currentInput && currentOperation) {
-            const num1 = parseFloat(previousInput);
-            const num2 = parseFloat(currentInput);
-            let result;
+        const num1 = parseFloat(previousInput);
+        const num2 = parseFloat(currentInput);
+        let result;
 
-            switch (currentOperation) {
-                case '+':
-                    result = num1 + num2;
-                    break;
-                case '-':
-                    result = num1 - num2;
-                    break;
-                case '*':
-                    result = num1 * num2;
-                    break;
-                case '/':
-                    result = num1 / num2;
-                    break;
-            }
-
-            currentInput = result.toString();
-            previousInput = "";
-            currentOperation = null;
+        switch (currentOperation) {
+            case '+': result = num1 + num2; break;
+            case '-': result = num1 - num2; break;
+            case '*': result = num1 * num2; break;
+            case '/': result = num1 / num2; break;
+            case 'pow': result = Math.pow(num1, num2); break;
         }
+
+        currentInput = result.toString();
+        previousInput = "";
+        currentOperation = null;
+    }
+
+    function factorial(n) {
+        if (n === 0 || n === 1) return 1;
+        let result = 1;
+        for (let i = 2; i <= n; i++) {
+            result *= i;
+        }
+        return result;
+    }
+
+    function updateDisplay() {
+        display.textContent = currentInput || '0';
     }
 });
