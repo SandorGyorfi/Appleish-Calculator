c8b60e4ac6b069edc7ee8e17befcae4e7e7908b1
Sandor Gyorfi <mr.sandorgyorfi@gmail.com>
2023-11-01 09:58:46 +0000

    Add
    Implemented initial JavaScript functionality for calculator: variables, event listeners, and button actions.

--- js/script.js
+++ js/script.js
@@ -2,3 +2,14 @@ let currentInput = "";
 let previousInput = "";
 let currentOperation = null;
 
+document.addEventListener('DOMContentLoaded', () => {
+    const display = document.querySelector('#display');
+    const buttons = document.querySelectorAll('#buttons button');
+
+    buttons.forEach(button => {
+        button.addEventListener('click', () => {
+            const type = button.dataset.type;
+            const value = button.dataset.value || button.textContent;
+            handleButtonClick(type, value);
+        });
+    });
